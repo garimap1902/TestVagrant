3052828aa08da658c737894a3b118701c9de8a32
Admin <Admin@LAPTOP-ROIFVC1J>
2022-10-28 13:58:27 +0530

    all test cases written

--- src/com/test/api/cricketteam/TestCricketTeam.java
+++ src/com/test/api/cricketteam/TestCricketTeam.java
@@ -1,5 +1,6 @@
 package com.test.api.cricketteam;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -22,9 +23,32 @@ public class TestCricketTeam {
 		JsonPath jdata = new JsonPath(data);
 		
 		int size = jdata.getInt("player.size()");
+		int noOfForeignCountries = 0;
+		for(int i=0;i<size;i++) {
+			String country = jdata.getString("player["+ i +"].country");
+			if(!country.equalsIgnoreCase("india")) {
+				noOfForeignCountries++;
+			}
+		}
 		
+		Assert.assertEquals(noOfForeignCountries, 4);
+	}
+	
+	@Test
+	public void testNoOfWicketKeepersInCricketTeam() {
+		String data = cricketTeamJson.getCricketTeamJson();
 		
+		JsonPath jdata = new JsonPath(data);
 		
+		int size = jdata.getInt("player.size()");
+		int noOfWicketKeepers = 0;
+		for(int i=0;i<size;i++) {
+			String country = jdata.getString("player["+ i +"].role");
+			if(!country.equalsIgnoreCase("Wicket-keeper")) {
+				noOfWicketKeepers++;
+			}
+		}
 		
+		Assert.assertTrue(noOfWicketKeepers >= 1);
 	}
 }
